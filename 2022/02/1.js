cbd870013fed4343f0779f37c3eb3ef9fd736d1f
Alfred Riesen <alfred.riesen@sinetcon.com>
2022-12-02 08:46:16 -0600

    2. day

---
+++ 2022/02/1.js
@@ -0,0 +1,58 @@
+import input from './input.js'
+
+let points = 0
+input.split('\n')
+    .forEach((line)=>{
+        let opponent
+        points += line.split(' ').reduce((acc, item, index)=>{
+            if(index === 0) {
+                opponent = item
+                return acc
+            }
+            switch (opponent) {
+                case 'A' : {
+                    switch (item) {
+                        case 'X' : {
+                            return 3 + 1;
+                        }
+                        case 'Y' : {
+                            return 6 + 2;
+                        }
+                        case 'Z' : {
+                            return 3;
+                        }
+                    }
+                    break
+                }
+                case 'B' : {
+                    switch (item) {
+                        case 'X' : {
+                            return 1;
+                        }
+                        case 'Y' : {
+                            return 3 + 2;
+                        }
+                        case 'Z' : {
+                            return 6 + 3;
+                        }
+                    }
+                    break
+                }
+                case 'C' : {
+                    switch (item) {
+                        case 'X' : {
+                            return 6 + 1;
+                        }
+                        case 'Y' : {
+                            return 2;
+                        }
+                        case 'Z' : {
+                            return 3 + 3;
+                        }
+                    }
+                }
+            }
+        },0)
+    })
+
+console.log(points)
