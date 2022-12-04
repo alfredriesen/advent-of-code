66ec4cb8f6c705b83be4c893319fae6ba1e56d69
Alfred Riesen <alfred.riesen@sinetcon.com>
2022-12-04 08:37:00 -0600

    4. day

---
+++ 2022/04/2.js
@@ -0,0 +1,22 @@
+import input from './input.js'
+
+let sum = 0
+input.split('\n')
+    .forEach((line) => {
+        let pairSections = line.split(',').map((elvSections) => {
+            let [start,end] = elvSections.split('-').map((number)=>parseInt(number))
+            let allElvSections = []
+            for (let i = start; i <= end; i++) {
+                allElvSections.push(i)
+            }
+            return allElvSections
+        })
+        if(
+            pairSections[0].some((section)=>pairSections[1].includes(section)) ||
+            pairSections[1].some((section)=>pairSections[0].includes(section))
+        ) {
+            sum++
+        }
+    })
+
+console.log(sum)
